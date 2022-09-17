8ad1d872ba7310a7fbde2788c759c0109d3bdeb4
Under3nder <marce.gnz303@gmail.com>
2022-09-17 21:39:51 +0200

    Hidden backend key

--- backend/Diagramm/settings.py
+++ backend/Diagramm/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+ENV = ".env"
+load_dotenv(ENV)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-82z0-)!p4*0$(ttml)-c0s*b41p8l^uwi-r7m=es@%kz8mld_y'
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
